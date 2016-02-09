dd491500584f267d4a0d1b0bb767fe8786b2bdf5
Erik rodriguez <erikgrl@hotmail.com>
2016-02-09 13:24:40 -0600

    Se agrego SWA con ajax

--- server/db.php
+++ server/db.php
@@ -1,6 +1,6 @@
 <?php 
 
-	$db = mysqli_connect('localhost', 'root', 'root', 'scotchbox');
+	$db = mysqli_connect('localhost', 'root', 'erik', 'gingin');
 	if(mysqli_connect_errno())
 	{
 		echo 'Failed to connect to MySQL: '.mysqli_connect_error();
--- server/sender.php
+++ server/sender.php
@@ -1,30 +1,30 @@
-<?php 
+<?php 	
+
 	require 'db.php';
 	require 'phpmailer/PHPMailerAutoload.php';
 	require 'phpmailer/class.pop3.php';
 
-	function enviaMailF2($name,$email,$mensaje){
-		//Envia Mail con link a F2
-		$templateUser = file_get_contents('mail.html');
-		$templateUser = str_replace('%name%', $name,$template);
-		$templateUser = str_replace('%email%', $email,$template);
-		$templateUser = str_replace('%hash%',$mensaje,$template);
+	/*function enviaMail($mail,$nombre,$mensaje){
+		//Envia Mail
 		$mail = new PHPMailer;
 
 		$mail->SMTPDebug = 3;
 
 		$mail->isSMTP();
-		$mail->Host = 'mail.quality.com.mx';
+		$mail->Host = '';
 		$mail->SMTPAuth = true;
 		$mail->Username = 'marketing@quality.com.mx';
 		$mail->Password = 'Accion559';
 		$mail->Port = 587;
 		$mail->setFrom('marketing@quality.com.mx','Contacto Quality');  
-		$mail->addAddress($email,$name,$mensaje);
+		$mail->addAddress($mail,$nombre,$mensaje);
 		$mail->isHTML(true);
 		$mail->CharSet = 'UTF-8';
 		$mail->Subject = 'Gracias por tu interés'; 
-		$mail->Body = $templateUser;
+		$mail->Body = "Alguien se ha registrado en el landing de Autoefectivo con los siguientes datos
+                          Nombre: ".$name."
+                          Email: ".$mail."
+                          Mensaje: ".$mensaje;
 
 		
 		if(!$mail->send()) {
@@ -32,32 +32,22 @@
 			echo 'Mailer Error: ' . $mail->ErrorInfo;
 
 		} else{
-			echo '<div id="respuestaAjax"><script type=\'text/javascript\'>
-				thankYou(); </script></div>';
+			echo 'mensaje enviado';
 		}
 
 	    }
-
-	//Mail checker
-	function checkMail($mail){
-		$db = mysqli_connect('localhost', 'root', 'erik', 'gingin');
-		if(preg_match("/^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/i", $mail)){
-			$sql = "SELECT `email` from users where `email` = '$mail';";
-			$result = mysqli_query($db, $sql);
-				if(mysqli_num_rows($result) == 0){
+	    */
+	    	function checkMail($mail){
+if(preg_match("/^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/i", $mail)){
 					return true;
 				}else{
 					return false;
 				}
-			
-		}else{
-			return false;
-		}
-	}
-
-
-	if(isset($_POST['formulario'])){
 				
+}				
+	      		
+
+
 				if(isset($_POST['nombre']) && !empty($_POST['nombre']) AND
 				   isset($_POST['mail']) && !empty($_POST['mail']) AND
 				   isset($_POST['mensaje']) && !empty($_POST['mensaje']) )
@@ -68,64 +58,46 @@
 					$nombre = mysqli_real_escape_string($db,$_POST['nombre']);
 					$mensaje = mysqli_real_escape_string($db,$_POST['mensaje']);
 					
-
-					
-					$sql1 = "INSERT INTO users (`id`,`nombre`,`mail`,`mensaje`) 
-					VALUES((SELECT `id` FROM general WHERE `email` = '$mail'),'$estado','$ciudad','$edad','$nivelestudios' ,'$ocupacion','$inversion','$membresia');";
-
-					$result = mysqli_query($db, $sql1);
-					
-						if($result){
-							echo "<div id='respuestaAjax'><script>sweetAlert('¡Gracias!', 'Hemos recibido correctamente tus datos', 'success'); $('form#f2 input, form#f2 select').attr('disabled','disabled'); </script></div>";
-						}else{
-							echo "<div id='respuestaAjax'><script>sweetAlert('Error', 'Ya hemos guardado tus datos', 'error'); </script></div>";
-						}
-
-				}else{
-					echo "<div id='respuestaAjax'><script>sweetAlert('Error', 'Los datos están incompletos', 'error'); </script></div>";
-				}
-
-	}else{
-
-
-					//Verifica datos ingresados
-					if(isset($_POST['nombre']) && !empty($_POST['nombre']) AND 
-					   isset($_POST['mail']) && !empty($_POST['mail']) AND
-					   isset($_POST['tel']) && !empty($_POST['tel'])){
-							
-							//Salvar en variables locales
-							$nombre= mysqli_real_escape_string($db,$_POST['nombre']);
-							$mail= mysqli_real_escape_string($db,$_POST['mail']);
-							$tel = mysqli_real_escape_string($db,$_POST['tel']);
-							$mensaje=mysqli_real_escape_string($db,$_POST['mensaje']);
-							$hash = sha1(rand(0,1000));
-
-
-							if(checkMail($mail)){
-								$sql = "INSERT INTO general (`id`, `nombre`, `email`, `tel`, `mensaje`, `hash`, `status`) VALUES('','$nombre','$mail','$tel','$mensaje','$hash','0')";
-								$saveDB = mysqli_query($db, $sql);
-									if($saveDB){
-										enviaMailF2($nombre,$mail,$hash);
-										//header('Location: ../thankyou.html');
-									}else{
-										echo "<div id='respuestaAjax'><script>sweetAlert('Error', 'Ocurrío un erro en la base de datos', 'error'); </script></div>";
-									}
-								
-							}else{
-
-								echo "<div id='respuestaAjax'><script>sweetAlert('Error', 'Este e-mail ya está en nuestra base de datos o es incorrecto', 'error'); </script></div>";
-							}
-					}else{
-						echo "<div id='respuestaAjax'><script>sweetAlert('Error', 'Los datos están incompletos', 'error'); </script></div>";
-					}
+    if(checkMail($mail)){
+	$sql = "INSERT INTO users (`id`, `nombre`, `email`, `mensaje`) VALUES('','$nombre','$mail','$mensaje')";
+    $saveDB = mysqli_query($db, $sql);
+	if($saveDB){
+	echo "<div id='respuestaAjax'><script>document.getElementById('formulario').reset(); </script> 
+							<script>sweetAlert('¡Gracias!','Datos guardados', 'success'); </script></div>";
 
 
 
+	//*enviaMail($nombre,$mail,$mensaje);
+    //header('Location: ../thankyou.html');
+    }
 
+	else{
+	echo "<div id='respuestaAjax'>
+						<script>sweetAlert('Error','Ocurrio un error en la base de datos','error'); </script></div>"; 
+	echo   mysqli_error($db);
+
+		}
 	}
+
+else{
+	echo "<div id='respuestaAjax'> 
+						 	<script>sweetAlert('Error','Error mail invalido','error'); </script></div>";
+								
+	}
+	}	
+
+	else{
+
+   echo "<div id='respuestaAjax'>
+		        <script>sweetAlert('Error','Campos vacios','error'); </script></div>";
+		 }
+
+		 
+
 
 
 
 
 
  ?>
+ 
